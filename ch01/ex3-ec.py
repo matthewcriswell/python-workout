56135bebebb996a5f6efe507e151bbed81950797
Matt Criswell <llewsircttam@gmail.com>
2023-01-24 17:49:01 -0600

    ex3 ec complete

--- ch01/ex3-ec.py
+++ ch01/ex3-ec.py
@@ -5,3 +5,16 @@ def floatfunc(base, before, after):
     after_str = str_base[point+1:point+after+1]
     post_str = before_str + '.' + after_str
     return float(post_str)
+
+def strfloat(base, base2):
+    str_base = str(base)
+    str_base2 = str(base2)
+
+    from decimal import Decimal
+    dec_base = Decimal(str_base)
+    dec_base2 = Decimal(str_base2)
+    dec_sum = dec_base + dec_base2
+
+    print(dec_sum)
+    return dec_sum
+    
