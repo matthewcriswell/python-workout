d5cf200a729e300900fc5a1582c6a9d17c9ca4bd
Matt Criswell <llewsircttam@gmail.com>
2023-01-24 20:31:40 -0600

    ex4-ec2 complete

--- ch01/ex4-ec2.py
+++ ch01/ex4-ec2.py
@@ -1,7 +1,8 @@
 def string_triangle(str_value):
-    str_list = list(str_value) 
-    for x, letter in enumerate(str_list):
-        print(letter * (x + 1))
+    ''' takes a string and makes a triangle out of the letters... sort of '''
+    str_list = list(str_value)
+    for amplitude, letter in enumerate(str_list):
+        print(letter * (amplitude + 1))
 
 def main():
     name = input("Enter your name: ")
