186914ce9e69811cf96648f51e4616fcdb750d5a
Matt Criswell <llewsircttam@gmail.com>
2023-01-24 19:40:12 -0600

    elif to dict hash

--- ch01/ex4.py
+++ ch01/ex4.py
@@ -1,23 +1,13 @@
 def hex_output(hex_num):
-    hex_num_list = list(hex_num)
+
+    hex_dict = {'0': 0, '1': 1, '2': 2, '3': 3, '4':4, '5':5, '6':6, '7':7, '8':8,'9':9, 'A':10, 'B':11, 'C':12, 'D':13, 'E':14, 'F':15}
+    hex_num_list = list(hex_num.upper())
     hex_num_list.reverse()
+
     iteration = 0
     total = 0
     for i in hex_num_list:
-        if i ==  'A':
-            num = 10
-        elif i == 'B':
-            num = 11
-        elif i == 'C':
-            num = 12
-        elif i == 'D': 
-            num = 13
-        elif i == 'E':
-            num = 14
-        elif i == 'F':
-            num = 15
-        else:
-            num = int(i)
+        num = hex_dict[i]
         total += num * (16 ** iteration)
         iteration += 1
     return total 
