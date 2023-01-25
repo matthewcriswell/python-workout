d45499d675b49ff128fbfa88f768aed455892507
Matt Criswell <llewsircttam@gmail.com>
2023-01-25 07:09:32 -0600

    ex5 first attempt

---
+++ ch02/ex5.py
@@ -0,0 +1,9 @@
+def pig_latin(eng_word):
+    ''' converts a lower case english word into pig latin '''
+    vowels_list = ['a', 'e', 'i', 'o', 'u']
+    if eng_word[0] in vowels_list:
+        return eng_word + 'way'
+    else:
+        return eng_word[1:] + eng_word[:1] + 'ay' 
+       
+
