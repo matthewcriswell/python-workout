e6a0d2ab624a43b1b79422f17777bf7c4347d54e
Matt Criswell <llewsircttam@gmail.com>
2023-01-25 07:17:25 -0600

    remove list for simple string check

--- ch02/ex5.py
+++ ch02/ex5.py
@@ -1,6 +1,5 @@
 def pig_latin(eng_word):
     ''' converts a lower case english word into pig latin '''
-    vowels_list = ['a', 'e', 'i', 'o', 'u']
-    if eng_word[0] in vowels_list:
+    if eng_word[0] in 'aeiou':
         return eng_word + 'way'
     return eng_word[1:] + eng_word[:1] + 'ay'
