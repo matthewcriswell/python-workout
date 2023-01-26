595feed2f0f7a625d765440a50582ceb9e41c0da
Matt Criswell <llewsircttam@gmail.com>
2023-01-25 19:45:40 -0600

    ex5-ec complete

--- ch02/ex5-ec.py
+++ ch02/ex5-ec.py
@@ -13,3 +13,15 @@ def pig_latin(word):
         pig_word = word[1:] + word[:1] + 'ay' + punct
         return pig_word.lower().capitalize()
     return word[1:] + word[:1] + 'ay' + punct
+
+def pig_latin2(word):
+    ''' another twist on pig latin conversion'''
+
+    vowel_set = set()
+    for letter in word:
+        if letter in 'aeiou':
+            vowel_set.add(letter)
+
+    if len(vowel_set) >= 2:
+        return f"{word}way"
+    return word[1:] + word[:1] + 'ay'
