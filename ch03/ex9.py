fd0e19dff27819d24681c202dc79d13ca803d82f
Matt Criswell <llewsircttam@gmail.com>
2023-01-27 15:11:00 -0600

    testing slice references in lists

--- ch03/ex9.py
+++ ch03/ex9.py
@@ -5,8 +5,21 @@ def firstlast(item):
         bort.append(item[0])
         bort.append(item[-1])
         return bort
-
     if type(item) == tuple:
         return (item[0],item[-1])
-
     return item[0] + item[-1]
+
+def firstlast2(sequence):
+    return sequence[:1] + sequence[-1:]
+
+
+def firstlast3(sequence):
+    print(f'----these are the sliced references----') 
+    print(f'Values: {sequence[:1]} and {sequence[-1:]}')
+    print(f'Types: {type(sequence[:1])} and {type(sequence[-1:])}')
+
+    print(f'----these are the direct references----') 
+    print(f'Values: {sequence[0]} and {sequence[-1:]}')
+    print(f'Types: {type(sequence[0])} and {type(sequence[-1:])}')
+
+    return sequence[:1] + sequence[-1:]
