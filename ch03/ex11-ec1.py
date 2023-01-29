70a8642e00fcab7ddeb86b54fe1f091b446e97ce
Matt Criswell <llewsircttam@gmail.com>
2023-01-28 19:02:28 -0600

    ex11 ec1

---
+++ ch03/ex11-ec1.py
@@ -0,0 +1,6 @@
+def sort_nums(sequence):
+    ''' sorts a sequence of positive and negative numbers '''
+    num_list = []
+    for num in sequence:
+        num_list.append(num)
+    return sorted(num_list)
