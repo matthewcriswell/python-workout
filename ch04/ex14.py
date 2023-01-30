60382a1a1a0c31e23f546b941b01e88c1ded6b80
Matt Criswell <llewsircttam@gmail.com>
2023-01-30 16:12:21 -0600

    ex14

---
+++ ch04/ex14.py
@@ -0,0 +1,28 @@
+
+MENU={
+'tea': 2,
+'cheeseburger': 5,
+'chocolate_shake': 5,
+'coke': 2,
+'salad': 4,
+'brownie': 3,
+'cookie': 2
+}
+possible_items = set(MENU.keys())
+live_order = True
+order_sum = 0
+user_input = ''
+while live_order:
+    user_input = input("What do you want to order? ")
+    if user_input == '':
+        print(f"That's that")
+        live_order = False
+    else:
+        if user_input in possible_items:
+            order_sum += MENU[user_input]
+            print(f"{user_input} costs {MENU[user_input]}. Order Total: {order_sum}")
+        else:
+            print("This is not something you order")
+            print(f"Pick one of: {possible_items}")
+
+print(f"Total: {order_sum}")
