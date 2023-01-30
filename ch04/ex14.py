ae70ca8f1280e9d7ffb326c32858b1c38f26561d
Matt Criswell <llewsircttam@gmail.com>
2023-01-30 16:16:43 -0600

    cleanup

--- ch04/ex14.py
+++ ch04/ex14.py
@@ -8,21 +8,26 @@ MENU={
 'brownie': 3,
 'cookie': 2
 }
-possible_items = set(MENU.keys())
-live_order = True
-order_sum = 0
-user_input = ''
-while live_order:
-    user_input = input("What do you want to order? ")
-    if user_input == '':
-        print(f"That's that")
-        live_order = False
-    else:
-        if user_input in possible_items:
-            order_sum += MENU[user_input]
-            print(f"{user_input} costs {MENU[user_input]}. Order Total: {order_sum}")
-        else:
-            print("This is not something you order")
-            print(f"Pick one of: {possible_items}")
 
-print(f"Total: {order_sum}")
+
+def restaurant():
+    possible_items = set(MENU.keys())
+    live_order = True
+    order_sum = 0
+    user_input = ''
+    while live_order:
+        user_input = input("What do you want to order? ").strip()
+        if not user_input:
+            print(f"That's that")
+            live_order = False
+        else:
+            if user_input in possible_items:
+                order_sum += MENU[user_input]
+                print(f"{user_input} costs {MENU[user_input]}. Order Total: {order_sum}")
+            else:
+                print("This is not something you order")
+                print(f"Pick one of: {possible_items}")
+    
+    print(f"Total: {order_sum}")
+
+restaurant()
