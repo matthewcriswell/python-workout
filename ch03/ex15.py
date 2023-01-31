ce6318672d54ae8cf3d9e7d6540986fa6f7c6a55
Matt Criswell <llewsircttam@gmail.com>
2023-01-30 21:34:37 -0600

    cleanup

--- ch03/ex15.py
+++ ch03/ex15.py
@@ -4,12 +4,12 @@ while True:
     user_input = input("Enter a city: ").strip()
     if user_input:
         precip = input("Enter rain amount:").strip()
-        if user_input in CITIES.keys():
+        if user_input in CITIES:
             CITIES[user_input] += int(precip)
         else:
             CITIES[user_input] = int(precip)
     else:
         break
 
-for city in CITIES:
-    print(f'{city}: {CITIES[city]:5}')
+for city, rainfall in CITIES.items():
+    print(f'{city}: {rainfall:5}')
