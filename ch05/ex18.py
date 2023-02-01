891954e398fa35998fa184b9cff24364a169295c
Matt Criswell <llewsircttam@gmail.com>
2023-02-01 06:28:13 -0600

    cleanup

--- ch05/ex18.py
+++ ch05/ex18.py
@@ -1,7 +1,8 @@
 def get_file_line(filename):
-    with open(filename, 'rt') as file:
-        for line in file.readlines(): 
-            last_line = line 
+    ''' opens a text file and returns the last line '''
+    with open(filename, 'rt', encoding="utf-8") as file:
+        for line in file.readlines():
+            last_line = line
     return last_line
 
 print(get_file_line('test.txt'), end='')
