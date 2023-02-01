b22b485c5a0932b27553ee68bdcb7c5ed71f4bef
Matt Criswell <llewsircttam@gmail.com>
2023-01-31 18:21:53 -0600

    ex16 cleanup

--- ch04/ex16.py
+++ ch04/ex16.py
@@ -1,23 +1,19 @@
 def dictdiff(d1, d2):
-
     print(f"d1: {d1}")
     print(f"d2: {d2}")
 
     diff_list = list(set(d1.items()) ^ set(d2.items()))
     if diff_list:
         print("There is a difference.")
-        #print(*diff_list)
-        #return diff_list
-
         temp_dict = {}
         if set([item[0] for item in diff_list]):
             for key in set([item[0] for item in diff_list]):
                 temp_dict.update({key:[d1.get(key), d2.get(key)]})
-                #print(f"{temp_dict}")
-                #print(f"{key}:[{d1.get(key)}, {d2.get(key)}]")
-        #set([item[1] for item in diff_list])
         print(f"{temp_dict}")
-    print("")
+        return temp_dict
+    print("There is no difference.")
+    return None
+
 
 d1 = {'a':1, 'b':2, 'c':3}
 d2 = {'a':1, 'b':2, 'c':4}
