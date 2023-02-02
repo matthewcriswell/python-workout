c4a22ee0efa04049416ab3d6ffd7a99d9e018f5d
Matt Criswell <llewsircttam@gmail.com>
2023-02-02 10:36:02 -0600

    ex19 ec3

--- ch05/ex19_ec123.py
+++ ch05/ex19_ec123.py
@@ -19,14 +19,18 @@ def passwd_to_dicts(filename):
     ]
     return passwd_entries
 
+
 passwd_list = passwd_to_dicts(FILENAME)
 
 user_shell_dict = {}
 for i in set([entry['shell'] for entry in passwd_list]):
-    user_shell_dict.update({i:[entry['username'] for entry in passwd_list if entry['shell'] == i]})
+    user_shell_dict.update({
+        i: [entry['username'] for entry in passwd_list if entry['shell'] == i]
+    })
 
 for i in user_shell_dict.keys():
     print(f"{i:<20} Users: {user_shell_dict.get(i)}")
+
 
 def factor_nums(nums):
     ''' returns a dict of factors from a list of nums '''
@@ -37,13 +41,37 @@ def factor_nums(nums):
             if x % i == 0:
                 numerator = x
                 denominator = i
-                quotient = int(numerator/denominator)
+                quotient = int(numerator / denominator)
                 if denominator not in output and quotient not in output:
                     output.append(denominator)
                     output.append(quotient)
-        output_dict.update({x:sorted(set((output)))})
+        output_dict.update({x: sorted(set((output)))})
     return output_dict
 
+
 #user_nums = input("Enter some integers separate by a space:").split()
-user_nums = [int(item) for item in input("Enter some integers separate by a space:").split()]
+user_nums = [
+    int(item)
+    for item in input("Enter some integers separate by a space:").split()
+]
 print(factor_nums(user_nums))
+
+# From /etc/passwd, create a dict in which the keys are the usernames (as in the main exercise) and the values are themselves dicts with keys (and appropriate values) for user ID, home directory, and shell.#
+
+#{username: {uid, home, shell}}
+# 'username', 'password', 'uid', 'gid', 'userinfo', 'home_dir', 'shell'
+
+user_info_dict = {}
+for i in [entry['username'] for entry in passwd_list]:
+    for item in [entry for entry in passwd_list if entry['username'] == i]:
+        uid = item['uid']
+        home_dir = item['home_dir']
+        shell = item['shell']
+        user_info_dict.update({
+            i: {
+                'uid': item['uid'],
+                'home_dir': item['home_dir'],
+                'shell': item['shell']
+            }
+        })
+print(user_info_dict)
