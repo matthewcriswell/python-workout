e155dce0c46661664129260a479b89dc8dca655f
Matt Criswell <llewsircttam@gmail.com>
2023-02-06 22:03:58 -0600

    ex24 updates

--- ch05/ex24.py
+++ ch05/ex24.py
@@ -5,7 +5,9 @@ conversion function
 
 def conversion_function(input_file, output_file):
     ''' reverses input file and writes to output file '''
-    with open(input_file, 'rt', encoding='UTF-8') as reader:
-        with open(output_file, 'wt', encoding='UTF-8') as writer:
-            for line in reader:
-                writer.writelines(''.join(line.strip()[::-1] + '\n'))
+    with open(input_file, 'rt',
+              encoding='UTF-8') as reader, open(output_file,
+                                                'wt',
+                                                encoding='UTF-8') as writer:
+        for line in reader:
+            writer.writelines(''.join(line.strip()[::-1] + '\n'))
