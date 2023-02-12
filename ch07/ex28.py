531c7ca0318db5cb363e589b305d9cb702a3bacb
Matt Criswell <llewsircttam@gmail.com>
2023-02-12 15:43:44 -0600

    ex28 improved

--- ch07/ex28.py
+++ ch07/ex28.py
@@ -5,8 +5,4 @@ make a CSV list string out of a range
 
 def join_numbers(num_range):
     ''' take a range and return a comma seperated list '''
-    nums_list = [str(i) for i in num_range]
-    last_num = nums_list.pop()
-    output_list = [i + ',' for i in nums_list]
-    output_list.append(last_num)
-    return ''.join(output_list)
+    return ','.join([str(i) for i in num_range])
