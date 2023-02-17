d7ccbde1f45271b4540b44b10fc82e57c675f8ba
Matt Criswell <llewsircttam@gmail.com>
2023-02-17 10:25:38 -0600

    ex34.py

--- ch07/ex34.py
+++ ch07/ex34.py
@@ -2,35 +2,71 @@
 find words containing all vowels
 '''
 
+# To profile:
+# 1. decorate function of interest with @profile
+# 2. kernprof -l ex34.py 
+# 3. python -m line_profiler ex34.py.lprof 
+
 from time import perf_counter
 
-VOWEL_HASH = {'a': 1, 'e': 2, 'i': 3, 'o': 4, 'u': 5}
+#VOWEL_HASH = {'a': 1, 'e': 2, 'i': 3, 'o': 4, 'u': 5}
+VOWEL_HASH = {
+    'a': 1,
+    'b': 0,
+    'c': 0,
+    'd': 0,
+    'e': 2,
+    'f': 0,
+    'g': 0,
+    'h': 0,
+    'i': 3,
+    'j': 0,
+    'k': 0,
+    'l': 0,
+    'm': 0,
+    'n': 0,
+    'o': 4,
+    'p': 0,
+    'q': 0,
+    'r': 0,
+    's': 0,
+    't': 0,
+    'u': 5,
+    'v': 0,
+    'w': 0,
+    'x': 0,
+    'y': 0,
+    'z': 0,
+    '-': 0
+}
 
 
+#@profile
 def get_sv(words_file):
     ''' find words with a, e, i, o, and u and return as a set '''
     with open(words_file, 'rt') as in_file:
-        words = in_file.readlines()
-    words = [word.strip() for word in words]
-    words_vowel_mode = []
+        words = [word.strip() for word in in_file.readlines()]
+
+    sv_words = []
     for word in words:
-        word_set = set(word.lower())
+        #word_set = set(word.lower())
         vowel_mode = 0
-        for char in word_set:
-            if char in 'aeiou':
-                vowel_mode += VOWEL_HASH[char]
-        words_vowel_mode.append({'word': word, 'vowel_mode': vowel_mode})
-    return set([
-        word['word'] for word in words_vowel_mode if word['vowel_mode'] == 15
-    ])
+        for char in set(word.lower()):
+            #if char in 'aeiou':
+            #    vowel_mode += VOWEL_HASH[char]
+            vowel_mode += VOWEL_HASH[char]
+        if vowel_mode == 15:
+            sv_words.append(word)
+
+    return set(sv_words)
 
 
+#@profile
 def get_sv2(filename):
     vowels = {'a', 'e', 'i', 'o', 'u'}
     return {
         word.strip()
-        for word in open(filename)
-        if vowels < set(word.lower())
+        for word in open(filename) if vowels < set(word.lower())
     }
 
 
