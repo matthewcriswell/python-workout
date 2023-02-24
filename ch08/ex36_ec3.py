46513561384cd26997a99dc6d390fc09ffe96be8
Matt Criswell <llewsircttam@gmail.com>
2023-02-24 16:36:59 -0600

    ex36 ec3

---
+++ ch08/ex36_ec3.py
@@ -0,0 +1,8 @@
+''' play around with dict.fromkeys() '''
+
+### I wasn't clear on the point of this exercise
+
+greek_letters = ['alpha', 'beta', 'gamma', 'delta', 'epsilon', 'zeta']
+for i in greek_letters:
+ print(dict.fromkeys(i,i))
+
