7b27b227bf3a6d09bb83c2f5b621a20d97ebf39c
Matt Criswell <llewsircttam@gmail.com>
2023-02-26 20:52:12 -0600

    ex37 ec3

---
+++ ch08/stuff.py
@@ -0,0 +1,14 @@
+''' import testing '''
+a = 1
+b = 2
+c = 3
+
+def foo():
+    ''' dummy func '''
+    print("foo")
+
+def bar():
+    ''' dummy func '''
+    print("bar")
+
+__all__ = ['a','c','bar']
