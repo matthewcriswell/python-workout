76ab974106af3c7291fcaaa534ef2117f80b2a2a
Matt Criswell <llewsircttam@gmail.com>
2023-02-28 23:04:12 -0600

    ex39 ec1

---
+++ ch09/ex39_ec1.py
@@ -0,0 +1,36 @@
+''' book and shelf classes '''
+
+
+class Book():
+    ''' defines a book object '''
+
+    def __init__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
+
+
+class Shelf():
+    ''' defines a shelf object as a collections of books '''
+
+    def __init__(self):
+        self.contents = []
+
+    def add_book(self, *books):
+        ''' add book(s) to contents '''
+        for book in books:
+            self.contents.append(book)
+
+    def total_price(self):
+        ''' print the total price of the bookshelf books '''
+        print(round(sum([book.price for book in self.contents]), 2))
+
+    def __repr__(self):
+        return '\n'.join(book.title for book in self.contents)
+
+
+b1 = Book('burps', 'matt criswell', 9.99)
+b2 = Book('fart', 'josh criswell', 19.99)
+b3 = Book('butt', 'seth thompson', 8.95)
+s = Shelf()
+s.add_book(b1, b2, b3, b1)
