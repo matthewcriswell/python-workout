8af97f2d57fcd3d4e5b46dc6f65e482bf39b05a3
Matt Criswell <llewsircttam@gmail.com>
2023-03-02 05:30:10 -0600

    ex40 ec1

---
+++ ch09/ex40_ec1.py
@@ -0,0 +1,10 @@
+''' define Person class with a population attribute '''
+
+
+class Person():
+    ''' defines a person '''
+    population = 0
+
+    def __init__(self, name):
+        self.name = name
+        Person.population += 1
