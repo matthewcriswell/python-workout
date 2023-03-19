233c791e5f496b86b43da07dc713f1f460651e3e
Matt Criswell <llewsircttam@gmail.com>
2023-03-18 23:09:52 -0500

    ex48 ec2

---
+++ ch10/ex48_ec2.py
@@ -0,0 +1,47 @@
+''' ex48: all lines, all files '''
+
+from os import listdir, stat
+from os.path import join
+
+MOST_LINES = 0
+
+
+def get_dir_files(directory):
+    ''' get a list of files in a directory '''
+    return [join(directory, file) for file in listdir(directory)]
+
+
+def get_file_descs(files_list):
+    ''' takes a list of file and returns a list of open files '''
+    return {file:open(file) for file in files_list}
+
+
+def get_lines(files):
+    output_str = ''
+    index = 0
+    file_no = 0
+    while index < MOST_LINES:
+        for file in files:
+            file_no += 1
+            try:
+                output_str = output_str + next(files[file])
+                yield (file, file_no, index, output_str)
+                output_str = ''
+            except:
+                pass
+        index += 1
+        file_no = 0
+
+
+directory = './test_dir'
+files = get_dir_files(directory)
+file_descs = get_file_descs(files)
+
+for file in file_descs:
+    most_lines = len(file_descs[file].readlines())
+    if most_lines > MOST_LINES:
+        MOST_LINES = most_lines
+    file_descs[file].seek(0)
+
+for line in get_lines(file_descs):
+    print(line)
